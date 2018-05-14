d66deb17b3ea3375d8f2fb5540ebde0343665ee3
Andrew McOlash <amcolash@gmail.com>
2018-05-14 15:51:20 -0700

    Rename peerflix server, update deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+cp ~/.npm-packages/lib/node_modules/peerflix-server/server/index.js peerflix-index.js
 npm run build
 ssh -t up-local "rm -rf ~/yify-react/*"
 scp -r build up-local:~/yify-react/
+scp peerflix-index.js up-local:~/index.js
+ssh -t up-local "docker cp ~/index.js peerflix:/home/app/server/ && rm ~/index.js && docker restart peerflix"
