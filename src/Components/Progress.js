902afb6ba4e63cd22d327ea1230183ba0fda5317
Andrew McOlash <amcolash@gmail.com>
2018-05-14 17:21:11 -0700

    Clean up progress code

--- src/Components/Progress.js
+++ src/Components/Progress.js
@@ -9,13 +9,14 @@ class Progress extends Component {
         const { torrent, openLink, cancelTorrent, fullName } = this.props;
         const name = fullName ? torrent.name : torrent.name.substring(0, torrent.name.indexOf(")") + 1);
         const speed = torrent.stats ? (torrent.stats.speed.down / 1000000).toFixed(2) : null;
+        const progress = torrent.progress[0].toFixed(0);
 
         return (
             <div className="progress">
                 <span>{name}</span>
-                <progress value={torrent.progress[0] > 1 ? torrent.progress[0] : null } max="100" />
-                <span>{torrent.progress[0].toFixed(0)}% </span>
-                {torrent.stats && torrent.progress[0] < 100 ? (
+                <progress value={progress > 1 ? progress : null } max="100" />
+                <span>{progress}% </span>
+                {torrent.stats && progress < 100 ? (
                     <span className={speed > 0.25 ? "green" : speed > 0.125 ? "orange" : "red"}>
                         {speed < 0.15 ? (
                             <FaExclamationCircle
