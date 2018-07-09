82126bcd54562c19ba78f8ec35a62fafb70134eb
Andrew McOlash <amcolash@gmail.com>
2018-07-09 00:54:11 -0700

    Change loading order

--- public/player/player.js
+++ public/player/player.js
@@ -1,18 +1,20 @@
-// Data is stored in the url (passed from main react app to this dummy page)
-// Decode the uri data and then parse out json
-var url = new URL(window.location.href);
-var data = url.searchParams.get("data");
-
-if (data) {
-    // Parse data from the url parameter
-    data = JSON.parse(decodeURIComponent(data));
-
-    var title = document.getElementById('title');
-    title.innerHTML = data.title;
-
-    var source = document.createElement('source');
-    source.setAttribute('src', data.url);
-
-    var video = document.getElementById('video');
-    video.appendChild(source);
+window.onload = function() {
+    // Data is stored in the url (passed from main react app to this dummy page)
+    // Decode the uri data and then parse out json
+    var url = new URL(window.location.href);
+    var data = url.searchParams.get("data");
+    
+    if (data) {
+        // Parse data from the url parameter
+        data = JSON.parse(decodeURIComponent(data));
+    
+        var title = document.getElementById('title');
+        title.innerHTML = data.title;
+    
+        var source = document.createElement('source');
+        source.setAttribute('src', data.url);
+    
+        var video = document.getElementById('video');
+        video.appendChild(source);
+    }
 }
